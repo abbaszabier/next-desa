5e05e0298b6fa4b22c6394a42a8d317a59f590a8
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-23 22:16:21 +0700

    init pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,24 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Desa",
   description: "Desa lorem ipsum",
+  manifest: "/manifest.json",
+  applicationName: "Desa App",
+  appleWebApp: {
+    statusBarStyle: "default",
+    capable: true,
+    title: "Desa App",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: "/images/icons/icon-192x192.png",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
