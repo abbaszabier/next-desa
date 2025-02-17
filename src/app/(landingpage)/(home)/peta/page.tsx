68ecc1b8a6bed2172cfa71284bcaca0bb80b6ea9
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-17 12:36:33 +0700

    update peta page

--- src/app/(landingpage)/(home)/peta/page.tsx
+++ src/app/(landingpage)/(home)/peta/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Peta() {
   return (
-    <div className="min-h-[20rem] flex items-center justify-center">peta</div>
+    <section className="flex flex-col items-center justify-center w-full px-4 lg:px-16 py-40">
+      <div className="mb-8 flex flex-col items-center justify-between md:mb-14 lg:mb-16">
+        <div className="text-center">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            className="text-4xl font-semibold tracking-tight text-balance text-gray-900 lg:text-7xl dark:text-gray-100"
+          >
+            Peta Desa
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            className="mt-4 mb-4 text-sm font-medium text-pretty text-gray-500 lg:text-xl/8"
+          >
+            Peta Desa Tapos I, Kecamatan Tenjolaya
+          </motion.p>
+        </div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.8 }}
+        className="w-full"
+      >
+        <div className="container grid items-center">
+          <div className="flex bg-muted">
+            <Image
+              src="https://www.shadcnblocks.com/images/block/placeholder-1.svg"
+              alt="placeholder hero"
+              className="h-[500px] w-full rounded-md object-cover"
+              width={0}
+              height={0}
+            />
+          </div>
+        </div>
+      </motion.div>
+    </section>
   );
 }
