3bc9c3e61ba9aea0fdcd76f715df016511a607aa
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-23 22:52:51 +0700

    feat: implement Berita component with dynamic table for news listing

--- src/app/dashboard/(home)/berita/page.tsx
+++ src/app/dashboard/(home)/berita/page.tsx
@@ -1,12 +1,21 @@
+import { UsersPrimaryButtons } from "../components/user-primary-button";
+import { DashboardBeritaTable } from "./table";
+
 export default function Berita() {
   return (
     <div className="flex flex-col flex-1 gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+      <div className="mb-2 flex flex-wrap items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">List Berita</h2>
+          <p className="text-muted-foreground">
+            Daftar berita yang telah dibuat oleh desa
+          </p>
+        </div>
+        <UsersPrimaryButtons title="Tambah Berita" />
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <DashboardBeritaTable />
+      </div>
     </div>
   );
 }
